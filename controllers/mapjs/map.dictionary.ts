35771fc91102bfbdf62999be19ad207712c3dc8c
adolecumberri <50199525+adolecumberri@users.noreply.github.com>
2020-12-10 23:41:39 +0100

    WIP maps

---
+++ controllers/mapjs/map.dictionary.ts
@@ -0,0 +1,25 @@
+import { connection } from '../../config/database';
+
+const A = 'a';
+
+const CIUDADES = [
+	{ id: 1, name: 'malaga', connections: [] },
+	{ id: 2, name: 'huelva', connections: [] },
+	{ id: 3, name: 'almería', connections: [] },
+	{ id: 4, name: 'albacete', connections: [] },
+	{ id: 5, name: 'murcia', connections: [] },
+	{ id: 6, name: 'ciudad real', connections: [] },
+	{ id: 7, name: 'badajoz', connections: [] },
+	{ id: 8, name: 'madrid', connections: [] },
+	{ id: 9, name: 'soria', connections: [] },
+	{ id: 10, name: 'zaragoza', connections: [] },
+	{ id: 11, name: 'asturias', connections: [] },
+	{ id: 12, name: 'barcelona', connections: [] },
+	{ id: 13, name: 'salamanca', connections: [] },
+	{ id: 14, name: 'bilbao', connections: [] },
+	{ id: 15, name: 'santiago de compostela', connections: [] },
+	{ id: 16, name: 'valencia', connections: [] },
+	{ id: 17, name: 'palencia', connections: [] },
+];
+
+export { CIUDADES };
---
+++ controllers/mapjs/map.ts
@@ -0,0 +1,8 @@
+import { CIUDADES } from './map.dictionary';
+
+export class Map {
+	constructor() {
+		this.CIUDADES = CIUDADES;
+	}
+	CIUDADES;
+}
