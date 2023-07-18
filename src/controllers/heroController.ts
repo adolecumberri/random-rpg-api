02e3ef60559b8d34ddbf6dac66339cdc07e353e9
adolecumberri <adolecumberri@gmail.com>
2023-07-18 18:55:18 +0200

    added new route

--- src/controllers/heroController.ts
+++ src/controllers/heroController.ts
@@ -5,10 +5,9 @@ import { HEROES_NAMES } from "../constants";
 import { heroFactory } from "../heroes/heroes";
 import { HeroIdentity } from "../types";
 
-const createHero = (req: Request, res: Response) => {
-    const { className, options }: { className: keyof typeof HEROES_NAMES, options?: HeroIdentity } = req.body;
+const createHero = (className: keyof typeof HEROES_NAMES, options: HeroIdentity) => {
 
-    let localClassName = className;
+    let localClassName = className?.toLocaleUpperCase() as keyof typeof HEROES_NAMES;
     // Verificar si el className es válido
     if (!(localClassName in HEROES_NAMES)) {
         //get random classname from HEROES_NAMES
@@ -18,18 +17,54 @@ const createHero = (req: Request, res: Response) => {
     console.log("localClassName", localClassName)
 
     // Crear el personaje usando la función correspondiente de heroFactory
-    try {
-        const createHeroFunc = heroFactory[localClassName];
-        const character = createHeroFunc(options);
+    const createHeroFunc = heroFactory[localClassName];
 
-        // Suponiendo que el personaje tiene un método para convertirse en JSON
-        return res.json(character);
+    // Suponiendo que el personaje tiene un método para convertirse en JSON
+    return createHeroFunc(options);
 
-    } catch (error) {
-        console.log(error);
+};
+
+
+const createHeroes = ( totalHeroes: number, heroTypes: { [x in keyof typeof HEROES_NAMES]?: number }) => {
+    const heroList: any[] = [];
+
+    // Verificar si se especificó el número total de héroes
+    if (!totalHeroes || totalHeroes === 0) {
+        // Si no se especificó, el número total de héroes será la suma de los valores en el objeto de tipos de héroes
+        totalHeroes = Object.values(heroTypes).reduce((sum, count) => sum + (count || 0), 0);
     }
+
+    // Verificar si la suma de los valores en el objeto de tipos de héroes es mayor que el número total de héroes
+    const totalHeroCount = Object.values(heroTypes).reduce((sum, count) => sum + (count || 0), 0);
+    if (totalHeroCount > totalHeroes) {
+        throw new Error('The sum of the hero types is greater than the total number of heroes');
+    }
+
+    // Crear héroes específicos según la lista de tipos y cantidades especificadas
+    for (const [heroType, count] of Object.entries(heroTypes)) {
+        if (count) {
+            for (let i = 0; i < count; i++) {
+                const createHeroFunc = heroFactory[heroType.toLocaleUpperCase() as keyof typeof HEROES_NAMES];
+                const character = createHeroFunc({} as HeroIdentity);
+                heroList.push(character);
+            }
+        }
+    }
+
+    // Crear héroes aleatorios para completar el número total de héroes
+    const remainingCount = totalHeroes - heroList.length;
+    for (let i = 0; i < remainingCount; i++) {
+        const randomHeroType = Object.keys(HEROES_NAMES)[Math.floor(Math.random() * Object.keys(HEROES_NAMES).length)];
+        const createHeroFunc = heroFactory[randomHeroType as keyof typeof HEROES_NAMES];
+        const character = createHeroFunc({} as HeroIdentity);
+        heroList.push(character);
+    }
+
+    return heroList;
 };
 
 export {
-    createHero
+    createHero,
+    createHeroes
 }
+// 
--- src/routes/heroRoutes.ts
+++ src/routes/heroRoutes.ts
@@ -1,11 +1,27 @@
 // routes/heroRoutes.ts
-import { Router } from 'express';
-import { createHero } from '../controllers';
-import { URL_CREATE } from '../constants';
+import { Router, Request, Response } from 'express';
+import { createHero, createHeroes } from '../controllers';
+import { HEROES_NAMES, URL_CREATE, URL_CREATE_MULTIPLE } from '../constants';
 
 const heroRouter = Router();
 
-heroRouter.post(URL_CREATE, createHero);
+heroRouter.post(URL_CREATE, (req: Request, res: Response) => {
+    const body = req.body;
+    const response = createHero(body.className, body.options);
+    res.json(response);
+});
+heroRouter.post(URL_CREATE_MULTIPLE, (req: Request, res: Response) => {
+    let { totalHeroes, heroTypes } = req.body;
+
+    const parsedHeroTypes: { [x in keyof typeof HEROES_NAMES]?: number } = JSON.parse(heroTypes);
+    totalHeroes = Number(totalHeroes);
+
+    try {
+        res.json(createHeroes(totalHeroes, parsedHeroTypes));
+    } catch (e: any) {
+        res.status(400).json({ error: e.message });
+    }
+});
 
 export {
     heroRouter
